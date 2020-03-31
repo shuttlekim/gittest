67a2a4027bdcb17b75b7b921b99d2eca02e724b8
shuttlekim <kunkun7762@gmail.com>
2020-03-31 15:09:12 +0900

    A가 다시 재수정합니다.

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -2,24 +2,22 @@ package com.example.demo.dao;
 
 public class MemberDao {
 
-	public String insertMember() {
-		String str = "ok";
+	public void insertMember() {
 		
 		System.out.println("출력문입니다.");
-		System.out.println("member");
 		
-		return str;		
+				
 	}
 	
 	public void updateMember() {
-<<<<<<< HEAD
-		System.out.println("회원의 정보를 없애버렸습니다.");
-  
+
+		System.out.println("회원의 정보를 수정했습니다..");
+		
 	}
 
-}
-=======
-		System.out.println("회원의 정보를 수정했습니다..");
+	public void deleteMember() {
+
+		System.out.println("회원의 정보를 없애버렸습니다.");
 	}
 }
->>>>>>> branch 'master' of https://github.com/shuttlekim/gittest.git
+
